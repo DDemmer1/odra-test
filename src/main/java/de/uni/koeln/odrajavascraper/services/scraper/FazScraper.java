feffae5d501eefdb9d69dd7ebf24bb1e44c77348
DDemmer1 <Dennis.Demmer@web.de>
2020-05-05 14:35:52 +0200

    update FazScraper

--- src/main/java/de/uni/koeln/odrajavascraper/services/scraper/FazScraper.java
+++ src/main/java/de/uni/koeln/odrajavascraper/services/scraper/FazScraper.java
@@ -3,6 +3,7 @@ package de.uni.koeln.odrajavascraper.services.scraper;
 import de.uni.koeln.odrajavascraper.entities.Article;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -45,10 +46,38 @@ public class FazScraper extends Scraper{
 
         Document doc = openURL(url);
 
+        //HEADLINE
         String headline = doc.body().getElementsByClass("atc-HeadlineText").text();
+        String subHeading = doc.body().getElementsByClass("atc-HeadlineEmphasis").text();
+        if(headline == null || headline == ""){
+            headline = doc.body().select("h2[class='h1']").text();
+        } else {
+            headline = subHeading + " " +headline;
+            headline = headline.replaceAll(":","-");
+        }
+
+        // TEXT BODY
         String textBody = doc.body().getElementsByClass("atc-TextParagraph").text();
+        if(textBody.length() < 10 || headline.length() < 1){
+            return null;
+        }
+
+        //AUTHOR
         String author = doc.body().getElementsByClass("atc-MetaAuthor").text();
-        String topic = doc.body().getElementsByClass("gh-LogoStage_Caption").text();
+
+        //TOPIC
+        String topic = "";
+        Elements topicElements = doc.body().getElementsByClass("gh-Hoverable-is-active");
+        if(topicElements.size() > 0){
+            topic = topicElements.get(0).getElementsByClass("gh-Ressort_LinkPlaceholder").text();
+        }
+
+        // CREATION DATE
+        String creationDate ="";
+        if(doc.body().getElementsByTag("time").size() > 0){
+            creationDate = doc.body().getElementsByTag("time").get(0).attr("title");
+            creationDate = creationDate.replaceAll("(?<=2020).*$","");
+        }
 
         Article article = new Article();
         article.setHeadline(headline);
@@ -56,10 +85,10 @@ public class FazScraper extends Scraper{
         article.setSourceName("faz");
         article.setTextBody(textBody);
         article.setCrawlDate(new Date());
-        article.setCreationDate(null);
         article.setAuthor(author);
         article.setLink(url);
         article.setTopic(topic);
+        article.setCreationDate(creationDate);
 
         return article;
     }
