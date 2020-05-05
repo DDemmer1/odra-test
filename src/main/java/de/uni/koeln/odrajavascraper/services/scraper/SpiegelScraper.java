edc59f3f951945775314c902e961f420ce22fb1a
DDemmer1 <Dennis.Demmer@web.de>
2020-05-05 14:36:21 +0200

    update spiegel scraper

--- src/main/java/de/uni/koeln/odrajavascraper/services/scraper/SpiegelScraper.java
+++ src/main/java/de/uni/koeln/odrajavascraper/services/scraper/SpiegelScraper.java
@@ -8,6 +8,7 @@ import com.rometools.rome.io.SyndFeedInput;
 import com.rometools.rome.io.XmlReader;
 import de.uni.koeln.odrajavascraper.entities.Article;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
 
@@ -27,30 +28,40 @@ public class SpiegelScraper extends Scraper {
         Document doc = openURL(url);
 
         //HEADLINE
-        String headline = doc.body().getElementsByClass("headline-intro").text() + " - " + doc.body().getElementsByClass("headline").text();
-        headline = headline.equals(" - ") ? "" : headline; // falls keine headline vorhanden ist und nur der Bidnestrich da steht
-        if (headline.contains("Exklusiv für Abonnenten")) { // falls eine PayWall existiert wird kein Artikel gespeichert
+        Elements headlineTag = doc.body().getElementsByTag("h2");
+        String headline = headlineTag.get(0).getElementsByTag("span").get(0).text() + " - " + headlineTag.get(0).getElementsByTag("span").get(1).text(); // falls keine headline vorhanden ist und nur der Bidnestrich da steht
+        if (headline.contains("Spiegel Plus") || headline.contains("Die Bilder des Tages")) { // falls eine PayWall existiert wird kein Artikel gespeichert
             return null;
         }
 
         //TEXTBODY
-        String textBody = doc.body().getElementsByTag("p").text();
+        String textBody ="";
+        Elements textElements = doc.body().getElementsByClass("clearfix lg:pt-32 md:pt-32 sm:pt-24 md:pb-48 lg:pb-48 sm:pb-32");
+        if(textElements != null){
+            textBody = textElements.text();
+        } else {
+            textBody = doc.body().getElementsByTag("p").text();
+        }
+
+        textBody = textBody.replaceAll(" Icon: Der Spiegel","");
+        textBody = textBody.replaceAll(" Melden Sie sich an und diskutieren Sie mit","");
+        if(textBody.length() < 10) return null;
 
         //AUTHOR
-        String author = doc.body().getElementsByClass("author").text();
+        String author = doc.body().getElementsByClass("text-black font-bold border-b border-shade-light hover:border-black").text();
         author = author.replaceAll("Von", "").replaceAll(" und ", ", "); // Autoren String wird gereinigt
 
         //TOPIC
-        Elements topicContainer = doc.body().getElementsByClass("current-channel-name");
+        Elements topicContainer = doc.body().getElementsByClass("relative mr-auto ml-4 text-white overflow-hidden pr-16 font-sans text-3xl ml-6 sm:overflow-x-auto sm:text-l sm:leading-normal sm:ml-4 tracking-wide whitespace-no-wrap");
         String topic = topicContainer.size() > 0 ? topicContainer.get(0).text() : "";
         if(topic.equals("Video")){
             return null;
         }
 
         //CREATIONDATE
-        String creationDate = doc.body().getElementsByClass("article-function-date").text();
+        String creationDate = doc.body().getElementsByTag("time").text();
 
-        creationDate = creationDate.replaceAll("(?<=2019).*$","");
+        creationDate = creationDate.replaceAll("(?<=2020).*$","");
         creationDate = creationDate.replaceAll("Montag|Dienstag|Mittwoch|Donnerstag|Freitag|Samstag|Sonntag|,|\\s", "");
 
 
@@ -82,6 +93,7 @@ public class SpiegelScraper extends Scraper {
             List<String> links = new ArrayList<>();
             for (SyndEntry entry : entries) {
                 links.add(entry.getLink());
+//                return links;
             }
             return links;
 
